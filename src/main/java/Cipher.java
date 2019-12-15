42bea49af8d9cc0bad927a87ac2f0dedca0c989b
mogere <pmnyatichi@gmail.com>
2019-12-15 19:13:20 +0300

    create and run test for instanciating cipher class

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,3 +1,9 @@
 public class Cipher {
+    private String initialText;
+    private int key;
 
+    public Cipher(String initialText, int key){
+        this.initialText = initialText;
+        this.key = key;
+    }
 }
--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 public class CipherTest {
     @Test
     public void  testCipher_instanciatesObject_object() {
-
+        Cipher testCipher =new Cipher("word", 5);
+        assertEquals(true, testCipher instanceof Cipher);
     }
 }
