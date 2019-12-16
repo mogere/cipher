334e27367dfbdf81e58863c8881b05a634643299
mogere <pmnyatichi@gmail.com>
2019-12-16 14:31:45 +0300

    sort display bug

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,10 @@
+import java.io.Console;
+
+
 public class App {
     public static void main(String[] args){
     Cipher cipher = new Cipher();
+    System.out.println("Welcome to Caesar Cipher application");
     cipher.setInitialText("This word");
     cipher.setKey(4);
     System.out.println(cipher.encrypt());
--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,6 +1,7 @@
 public class Cipher {
     private String initialText;
     private int key;
+    private String encrypted = "";
 
     public Cipher(String initialText, int key){
         this.initialText = initialText;
@@ -24,22 +25,41 @@ public class Cipher {
     }
 
     public void setKey(int key){
-        this.key = key;
+        if (key>26){
+            this.key = key%26;
+        }
+        else if (key<0){
+            this.key = (key%26)+26;
+        }
+        else
+            this.key = key;
+
     }
 
     public String encrypt(){
-        String encrypted = "";
+
         for(int i=0, length = initialText.length();i<length;i++ ) {
             char check = initialText.charAt(i);
             if (Character.isLetter(check)) {
                 if (Character.isLowerCase(check)) {
                     char checked = encryption(check);
+                    if (checked>'z'){
+                        encrypted += (char)(check - (26 - key));
+                    }
+                    else
                     encrypted += checked;
+
                 } else if (Character.isUpperCase(check)) {
-                    char checked = encryption(check);;
-                    encrypted += checked;
+                    char checked = encryption(check);
+                    if (checked>'Z'){
+                        encrypted += (char)(check - (26 - key));
+                    }
+                    else
+                        encrypted += checked;
                 }
             }
+            else
+                encrypted += check;
         }
 
         return encrypted;
@@ -50,23 +70,35 @@ public class Cipher {
         return checked;
     }
 
+
     public String decrypt(){
         String decrypted = "";
-        for(int i=0, length = initialText.length();i<length;i++ ) {
-            char check = initialText.charAt(i);
+        for(int i=0, length = encrypted.length();i<length;i++ ) {
+            char check = encrypted.charAt(i);
             if (Character.isLetter(check)) {
                 if (Character.isLowerCase(check)) {
                     char checked = decryption(check);
-                    decrypted += checked;
+                    if (checked<'a'){
+                        decrypted += (char)(check + (26 - key));
+                    }
+                    else
+                        decrypted += checked;
+
                 } else if (Character.isUpperCase(check)) {
-                    char checked = decryption(check);;
-                    decrypted += checked;
+                    char checked = decryption(check);
+                    if (checked>'Z'){
+                        decrypted += (char)(check + (26 - key));
+                    }
+                    else
+                        decrypted += checked;
                 }
             }
+            else
+                decrypted += check;
         }
-
         return decrypted;
     }
+
     public char decryption(char check){
         char checked = (char)(check - key);
         return checked;
