f09a4ef8ab8092ae98185e43555c7192eeaa762e
mogere <pmnyatichi@gmail.com>
2019-12-16 14:49:02 +0300

    Add User interface and blank readme file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,24 @@
 import java.io.Console;
+import java.util.Scanner;
 
 
 public class App {
     public static void main(String[] args){
     Cipher cipher = new Cipher();
+    Scanner input = new Scanner(System.in);
+    //Ask for user input
     System.out.println("Welcome to Caesar Cipher application");
-    cipher.setInitialText("This word");
-    cipher.setKey(4);
-    System.out.println(cipher.encrypt());
-    System.out.println(cipher.decrypt());
+    System.out.println("------------------------------------");
+    System.out.println("Enter the text you would like encrypted:");
+
+    //receive user input for text to encrypt
+    String toEncrypt = input.nextLine();
+    cipher.setInitialText(toEncrypt); //pass it to cipher class
+    System.out.println("Enter the Key you would like the text to be encrypted by:");
+    int key = Integer.parseInt(input.nextLine());
+    cipher.setKey(key);
+    System.out.println("Your Encrypted text will be: " + cipher.encrypt());
+    System.out.println("If decrypted, it will be back to: " + cipher.decrypt());
+
     }
 }
